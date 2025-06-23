d53c969175c481a138f7ff5c6988ba718fe8b878
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-06-23 16:02:35 -0700

    Update

--- src/main/java/org/yearup/EasyshopApplication.java
+++ src/main/java/org/yearup/EasyshopApplication.java
@@ -8,6 +8,7 @@ public class EasyshopApplication
 {
 
     public static void main(String[] args) {
+        System.out.println("TH");
         SpringApplication.run(EasyshopApplication.class, args);
     }
 
